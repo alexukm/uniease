8c2f7c7d655dcd3a52b4b7629f37df5a02850304
alexukm <a172569@siswa.ukm.edu.my>
2023-08-15 01:24:11 +0800

    modify: chatRoom 重试调整

--- src/screens/ChatRoom.js
+++ src/screens/ChatRoom.js
@@ -1,81 +1,84 @@
 import React from "react";
-import {Composer, GiftedChat, InputToolbar} from "react-native-gifted-chat";
-import {format} from "date-fns";
-import {useDispatch, useSelector} from "react-redux";
-import { addMessage, selectChatMessage} from "../com/evotech/common/redux/chatSlice";
+import { Composer, GiftedChat, InputToolbar } from "react-native-gifted-chat";
+import { format } from "date-fns";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage, selectChatMessage } from "../com/evotech/common/redux/chatSlice";
 import uuid from "react-native-uuid";
-import {UserChat} from "../com/evotech/common/redux/UserChat";
+import { UserChat } from "../com/evotech/common/redux/UserChat";
 import {
-    clientStatus,
-    whenConnect,
+  clientStatus,
+  whenConnect,
 } from "../com/evotech/common/websocket/SingletonWebSocketClient";
-import {SafeAreaView} from "react-native";
+import { SafeAreaView } from "react-native";
 
-export default function ChatRoom({route}) {
-    const {receiverName, receiverUserCode,orderId,receiverOrderId} = route.params;
-    const dispatch = useDispatch();
+export default function ChatRoom({ route }) {
+  const { receiverName, receiverUserCode, orderId, receiverOrderId } = route.params;
+  const dispatch = useDispatch();
 
-    const messages = useSelector(selectChatMessage);
+  const messages = useSelector(selectChatMessage);
 
-    async function onSend(newMessages = []) {
-        try {
-            const param = {
-                receiverName: receiverName,
-                receiverOrderId: receiverOrderId,
-                receiverUserCode: receiverUserCode,
-                message: newMessages[0].text,
-                requestTime: format(new Date(), "yyyy-MM-dd HH:mm:ss"),
-            };
+  const sleep = new Promise(resolve => setTimeout(resolve, 0.5));
 
-            const message = {
-                _id: uuid.v4(),
-                userCode: receiverUserCode,
-                text: newMessages[0].text,
-                orderId: orderId,
-                createdAt: param.requestTime,
-                user: {
-                    _id: 1,
-                    name: receiverName,
-                },
-            };
-            // 连接被异常关闭
-            if (!clientStatus()) {
-                await UserChat(true).then();
-                if (!clientStatus()) {
-                    alert("Connection is not established,Please try again later");
-                    return;
-                }
-            }
-            await whenConnect((socketClient) => {
-                socketClient.publish({destination: "/uniEase/v1/order/chat/ride", body: JSON.stringify(param)});
-            });
-            dispatch(addMessage(message));
-        } catch (e) {
-            alert(e.message);
+  async function onSend(newMessages = []) {
+    try {
+      const param = {
+        receiverName: receiverName,
+        receiverOrderId: receiverOrderId,
+        receiverUserCode: receiverUserCode,
+        message: newMessages[0].text,
+        requestTime: format(new Date(), "yyyy-MM-dd HH:mm:ss"),
+      };
+
+      const message = {
+        _id: uuid.v4(),
+        userCode: receiverUserCode,
+        text: newMessages[0].text,
+        orderId: orderId,
+        createdAt: param.requestTime,
+        user: {
+          _id: 1,
+          name: receiverName,
+        },
+      };
+      // 连接被异常关闭
+      if (!clientStatus()) {
+        await UserChat(true).then();
+        await sleep;
+        if (!clientStatus()) {
+          alert("Send failed,Please try again!");
+          return;
         }
+      }
+      await whenConnect((socketClient) => {
+        socketClient.publish({ destination: "/uniEase/v1/order/chat/ride", body: JSON.stringify(param) });
+      });
+      dispatch(addMessage(message));
+    } catch (e) {
+      alert(e.message);
     }
+  }
 
-    return (
-        <SafeAreaView style={{flex: 1, justifyContent: "space-between", paddingBottom: 30}}>
-            <GiftedChat
-                messages={messages[orderId] || []}
-                onSend={newMessages => onSend(newMessages)}
-                user={{_id: 1}}
-                renderInputToolbar={(props) => (
-                    <InputToolbar {...props}
-                                  renderComposer={(composerProps) => (
-                                      <Composer
-                                          {...composerProps}
-                                          textInputStyle={{
-                                              ...composerProps.textInputStyle,
-                                              color: "black"
-                                          }} // 这里设置输入框文字颜色为黑色
-                                      />
-                                  )}
-                    />
-                )}
-            />
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: "space-between", paddingBottom: 30 }}>
+      <GiftedChat
+        messages={messages[orderId] || []}
+        onSend={newMessages => onSend(newMessages)}
+        user={{ _id: 1 }}
+        renderInputToolbar={(props) => (
+          <InputToolbar {...props}
+                        renderComposer={(composerProps) => (
+                          <Composer
+                            {...composerProps}
+                            textInputStyle={{
+                              ...composerProps.textInputStyle,
+                              color: "black",
+                            }} // 这里设置输入框文字颜色为黑色
+                          />
+                        )}
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
 }
 
